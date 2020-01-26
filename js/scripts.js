dd4b7bb4aff2272ce2a3647045eb179ed83ff6e4
Marc Davies <marcdaviesriot@gmail.com>
2020-01-25 16:16:05 -0800

    parse userInput after validation and save as number

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,15 @@ $(function() {
     console.log("userInput", userInput);
     // Check for empty field
     if (userInput === "") {
-        $("#output").text("This field is required.");
-        return false;
+      $("#output").text("This field is required.");
+      return false;
+      // Check for non-numerical entries
     } else if (isNaN(userInput)) {
-        $("#output").text("Please enter a number.");
-    };
+      $("#output").text("Please enter a number.");
+    } else {
+      //If non-empty and numerical, parse userInput
+      let number = parseFloat(userInput);
+      console.log("number", number);
+    }
   });
 });
