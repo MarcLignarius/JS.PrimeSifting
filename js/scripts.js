ac036702e550dd55be74a1a5f298305076bf2941
Marc Davies <marcdaviesriot@gmail.com>
2020-01-25 16:05:13 -0800

    retrieve user input from form, save it as userInput, log it to console

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(function() {
-
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    let userInput = $("input#userInput").val();
+    console.log("userInput", userInput);
+  });
 });
