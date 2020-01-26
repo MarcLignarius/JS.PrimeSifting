3d2d00c90d9d32f36ef3e9bb0f257e460234ce75
Marc Davies <marcdaviesriot@gmail.com>
2020-01-25 16:10:23 -0800

    add form validation, check for empty field

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,10 @@ $(function() {
     event.preventDefault();
     let userInput = $("input#userInput").val();
     console.log("userInput", userInput);
+    // Check for empty field
+    if (userInput === "") {
+        $("#output").text("Please enter a number.");
+        return false;
+    };
   });
 });
