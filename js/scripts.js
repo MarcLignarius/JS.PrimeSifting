5fc413333ecda9c05158c8a8865b5bb5672f6061
Marc Davies <marcdaviesriot@gmail.com>
2020-01-26 13:44:46 -0800

    add removeNonPrimes function WIP

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,24 @@ $(function() {
       console.log("number", number);
       let numArray = createNumArray(number);
       console.log("numArray", numArray);
+      let primeNumbers = removeNonPrimes(numArray);
+      console.log("primeNumbers", primeNumbers);
     }
   });
 
   function createNumArray(number) {
-      let numArray = [];
-      for (i = 2; i <= number; i++) {
-          numArray.push(i);
-      }
-      return numArray;
+    let numArray = [];
+    for (i = 2; i <= number; i++) {
+      numArray.push(i);
+    }
+    return numArray;
+  }
+
+  function removeNonPrimes(numArray) {
+    let primeNumbers = [];
+    for (i = 2; i <= numArray.length+1; i++) {
+      primeNumbers.push(i);
+    }
+    return primeNumbers;
   }
 });
